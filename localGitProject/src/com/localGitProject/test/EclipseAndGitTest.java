43c190ae8069c5554ab9098264caa486ef54eee2
qinjunhui <74575296+qinjunhui@users.noreply.github.com>
2020-12-04 12:45:57 +0800

    Update EclipseAndGitTest.java

--- localGitProject/src/com/localGitProject/test/EclipseAndGitTest.java
+++ localGitProject/src/com/localGitProject/test/EclipseAndGitTest.java
@@ -3,6 +3,8 @@ package com.localGitProject.test;
 public class EclipseAndGitTest {
 	public static void main(String[] args){
 		System.out.print("Hello Git!");
-	        System.out.print("Hello Git!");
+		
+		
+	        System.out.print("Hello LocalHost!");
 	}
 }
